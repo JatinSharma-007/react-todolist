7fdb16677fd3a350f182bdb9356270937a646a63
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2021-08-01 00:01:06 +0530

    progressing in react

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import React , {useState}from 'react';
+import { useState } from 'react';
+
 import './App.css';
-// Importing components 
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
-
 function App() {
-  const [inputText , setInputText] = useState("");
-  const [todos , setTodos] = useState ([
 
-
-  ]);
-
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <header>
-        <h1>To Do List </h1>
-      </header>
-      <Form 
-      inputText={inputText}
-      todos={todos} 
-      setTodos={setTodos} 
-      setInputText={setInputText}/>
-      <TodoList setTodos={setTodos} todos={todos} />
+        <header>
+          <h1>REACT APP</h1>
+        </header>
+        <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+        <TodoList todos={todos}/>
     </div>
-    
   );
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,15 @@
-import React   from 'react' ;
-//Importing Components
 import Todo from './Todo';
 
-const TodoList = ({todos , setTodos}) => {
+const TodoList = ({todos}) => {
+  return (
+      <div className="todo-container">
+        <ul className="todo-list">
+          {todos.map(todo => (
+            <Todo text={todo.text}/>
+          ))}
+        </ul>
+      </div>
+  );
+};
 
-    return (
-        <div className="todo-container">
-            <ul className="todo-list">
-                {todos.map(todo => (
-                    <Todo setTodos={setTodos}
-                     todos={todos}
-                     todo={todo}
-                     key={todo.id} 
-                     text={todo.text}/>
-                ))}
-            </ul>
-        </div>   
-    )
-}
-
-export default TodoList;
+export default TodoList
